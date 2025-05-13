d9f146bc20d91aefa44ac2cda533615e9477d2b1
Your Name <m-martemyanov2004@mail.ru>
2025-05-13 21:46:53 +0300

    добавили новые route запросы в папку routes

--- routes/web.php
+++ routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/home', function () {
+   return csrf_token();
+});
+
+Route::post('/home', function () {
+   return csrf_token();
+});
